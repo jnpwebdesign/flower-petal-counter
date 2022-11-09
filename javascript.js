8b427ad86e95abf4b3e26608af5ae25bd6725c27
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-11-08 21:56:48 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let welcomeElement = document.getElementById("welcome-element");
+let name1 = "Julie";
+let greeting = "Good morning, ";
+welcomeElement.textContent = greeting + name1;
+welcomeElement.textContent += "!";
+
+let count = 0;
+let countElement = document.getElementById("count-element");
+
+function increment() {
+    count++;
+    countElement.textContent = count;
+}
+
+let saveElement = document.getElementById("save-element");
+
+function save() {
+    let printout = " " + count + " -";
+    saveElement.textContent += printout;
+    count = 0;
+    countElement.textContent = 0;
+    
+
+}
+
+
